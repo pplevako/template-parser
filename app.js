6f49b575987c6a0a71505ee4fe3579877795026b
renpika <kaminapoke@gmail.com>
2014-01-24 00:06:32 +0400

    Launching example become more complex. More complexity for the Complexity God!

--- app.js
+++ app.js
@@ -5,24 +5,31 @@ var render = require('./render');
 
 var template = "<table>\n" +
   "{# This is a comment#}" +
-  "{{category}}\n " +
   "{% table %}" +
-  "   <tr>\n" +
-  "      {% . %}" +
-  "<td>{{ . }}  {{ . }} {{category}}</td>" +
+  "  <tr>\n" +
+  "    {% . %}" +
+  "<td>" +
+  "{{fruits}}" +
+  "{% nested %}" +
+  " {{ . }}" +
   "{% / %}" +
-  "\n   </tr>\n" +
+  " {{ . }}" +
+  "</td>" +
+  "{% / %}\n" +
+  "  </tr>\n" +
   "{% / %}" +
   "</table>\n";
 
+console.log("Template:\n" + template);
+
 var data = {
-  category: "Fruits",
+  fruits: "Fruits",
   table: [
     [1, 2, 3],
     [4, 5, 6]
   ],
-  test: [7, 8, 9]
+  nested: ["are", "nested"]
 };
 
 var result = render(template, data);
-console.log(result);
+console.log("Result:\n" + result);
